0c06476dda46219eeb7fdbc187434b275b4776df
weirdoza <whatkai@yahoo.com>
2016-08-02 11:11:07 -0400

    s
    here

--- app/src/main/java/com/ltree/student/expenses/ExpensesListActivity.java
+++ app/src/main/java/com/ltree/student/expenses/ExpensesListActivity.java
@@ -9,5 +9,6 @@ public class ExpensesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // test git ?
     }
 }
